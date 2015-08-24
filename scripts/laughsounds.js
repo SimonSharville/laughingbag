aa224d1702681ac7e75769fa7aa8689ff80c4854
Simon Sharville <design@simonsharville.co.uk>
2015-08-24 14:18:10 +0100

    Starting Files
    These are the original files copied over from
    testarea/sounds/soundtest02

---
+++ scripts/laughsounds.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+  $(document).ready(function(){
+         
+   $('#LaughA01').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A01",
+     volume:0.9
+   });
+   
+   $('#LaughA02').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A02",
+     volume:0.9
+   });
+
+
+  });
