336e73ee079487ef4909e55146a46364ef19539a
Simon Sharville <design@simonsharville.co.uk>
2015-08-25 16:57:02 +0100

    Added the B Laughs

--- assets/scripts/laughsounds.js
+++ assets/scripts/laughsounds.js
@@ -50,6 +50,67 @@
      volume:0.9
    });
 
+   $('#LaughB01').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B01",
+     volume:0.9
+   });
+
+   $('#LaughB02').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B02",
+     volume:0.9
+   });
+
+   $('#LaughB03').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B03",
+     volume:0.9
+   });
+
+   $('#LaughB04').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B04",
+     volume:0.9
+   });
+
+   $('#LaughB05').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B05",
+     volume:0.9
+   });
+
+   $('#LaughB06').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B06",
+     volume:0.9
+   });
+
+   $('#LaughB07').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B07",
+     volume:0.9
+   });
+
+   $('#LaughB08').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B08",
+     volume:0.9
+   });
+
+   // Removed the baby laugh
+   // $('#LaughB09').audioeffects({
+   //   theTrigger: "mouseover click",
+   //   audioId: "B09",
+   //   volume:0.9
+   // });
+
+   $('#LaughB10').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "B10",
+     volume:0.9
+   });
+
 
 
 
