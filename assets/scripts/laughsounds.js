f7b0c4b39083c112e6eb8bb70a3e8baeb29be528
Simon Sharville <design@simonsharville.co.uk>
2015-08-25 16:15:57 +0100

    Added A-list Sounds
    Added all sounds and bubbles with the A prefix

--- assets/scripts/laughsounds.js
+++ assets/scripts/laughsounds.js
@@ -14,5 +14,45 @@
      volume:0.9
    });
 
+   $('#LaughA03').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A03",
+     volume:0.9
+   });
+
+   $('#LaughA04').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A04",
+     volume:0.9
+   });
+
+   $('#LaughA05').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A05",
+     volume:0.9
+   });
+
+   $('#LaughA06').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A06",
+     volume:0.9
+   });
+
+   $('#LaughA07').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A07",
+     volume:0.9
+   });
+
+   $('#LaughA08').audioeffects({
+     theTrigger: "mouseover click",
+     audioId: "A08",
+     volume:0.9
+   });
+
+
+
+
+
 
   });
