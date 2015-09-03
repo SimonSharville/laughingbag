46741669bf636811360d66c6dd19f506b1f765ea
Simon Sharville <design@simonsharville.co.uk>
2015-09-03 18:12:55 +0100

    Added Credits Page
    Test 22 Added and styled Credits page, still need some tweaks though.

---
+++ assets/scripts/sound-mouseover.js
@@ -0,0 +1,595 @@
+// JavaScript Document
+
+/* A01 ===============================================================*/
+
+function playclipA01() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA01.src = "audio/A01.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A01")[0];
+audio.play();
+}
+}
+}
+
+/* A02 ===============================================================*/
+
+function playclipA02() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA02.src = "audio/A02.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A02")[0];
+audio.play();
+}
+}
+}
+
+/* A04 ===============================================================*/
+
+function playclipA03() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA03.src = "audio/A03.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A03")[0];
+audio.play();
+}
+}
+}
+
+
+/* A04 ===============================================================*/
+
+function playclipA04() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA04.src = "audio/A04.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A04")[0];
+audio.play();
+}
+}
+}
+
+
+/* A05 ===============================================================*/
+
+function playclipA05() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA05.src = "audio/A05.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A05")[0];
+audio.play();
+}
+}
+}
+
+
+/* A06 ===============================================================*/
+
+function playclipA06() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA06.src = "audio/A06.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A06")[0];
+audio.play();
+}
+}
+}
+
+
+/* A07 ===============================================================*/
+
+function playclipA07() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA07.src = "audio/A07.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A07")[0];
+audio.play();
+}
+}
+}
+
+
+
+/* A08 ===============================================================*/
+
+function playclipA08() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundA08.src = "audio/A08.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("A08")[0];
+audio.play();
+}
+}
+}
+
+
+
+
+/* B01 ===============================================================*/
+
+function playclipB01() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB01.src = "audio/B01.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B01")[0];
+audio.play();
+}
+}
+}
+
+
+
+/* B02 ===============================================================*/
+
+function playclipB02() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB02.src = "audio/B02.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B02")[0];
+audio.play();
+}
+}
+}
+
+
+/* B03 ===============================================================*/
+
+function playclipB03() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB03.src = "audio/B03.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B03")[0];
+audio.play();
+}
+}
+}
+
+
+/* B04 ===============================================================*/
+
+function playclipB04() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB04.src = "audio/B04.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B04")[0];
+audio.play();
+}
+}
+}
+
+/* B05 ===============================================================*/
+
+function playclipB05() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB05.src = "audio/B05.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B05")[0];
+audio.play();
+}
+}
+}
+
+
+/* B06 ===============================================================*/
+
+function playclipB06() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB06.src = "audio/B06.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B06")[0];
+audio.play();
+}
+}
+}
+
+
+
+/* B07 ===============================================================*/
+
+function playclipB07() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB07.src = "audio/B07.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B07")[0];
+audio.play();
+}
+}
+}
+
+
+/* B08 ===============================================================*/
+
+function playclipB08() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB08.src = "audio/B08.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B08")[0];
+audio.play();
+}
+}
+}
+
+
+/* B09 ===============================================================*/
+
+function playclipB09() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB09.src = "audio/B09.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B09")[0];
+audio.play();
+}
+}
+}
+
+
+/* B10 ===============================================================*/
+
+function playclipB10() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundB10.src = "audio/B10.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("B10")[0];
+audio.play();
+}
+}
+}
+
+
+/* C01 ===============================================================*/
+
+function playclipC01() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC01.src = "audio/C01.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C01")[0];
+audio.play();
+}
+}
+}
+
+
+/* C02 ===============================================================*/
+
+function playclipC02() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC02.src = "audio/C02.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C02")[0];
+audio.play();
+}
+}
+}
+
+
+/* C03 ===============================================================*/
+
+function playclipC03() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC03.src = "audio/C03.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C03")[0];
+audio.play();
+}
+}
+}
+
+
+/* C04 ===============================================================*/
+
+function playclipC04() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC04.src = "audio/C04.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C04")[0];
+audio.play();
+}
+}
+}
+
+
+/* C05 ===============================================================*/
+
+function playclipC05() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC05.src = "audio/C05.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C05")[0];
+audio.play();
+}
+}
+}
+
+
+/* C06 ===============================================================*/
+
+function playclipC06() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC06.src = "audio/C06.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C06")[0];
+audio.play();
+}
+}
+}
+
+/* C07 ===============================================================*/
+
+function playclipC07() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC07.src = "audio/C07.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C07")[0];
+audio.play();
+}
+}
+}
+
+/* C08 ===============================================================*/
+
+function playclipC08() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundC08.src = "audio/C08.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("C08")[0];
+audio.play();
+}
+}
+}
+
+/* D01 ===============================================================*/
+
+function playclipD01() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundD01.src = "audio/D01.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("D01")[0];
+audio.play();
+}
+}
+}
+
+/* D02 ===============================================================*/
+
+function playclipD02() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundD02.src = "audio/D02.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("D02")[0];
+audio.play();
+}
+}
+}
+
+
+/* D03 ===============================================================*/
+
+function playclipD03() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundD03.src = "audio/D03.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("D03")[0];
+audio.play();
+}
+}
+}
+
+
+/* D04 ===============================================================*/
+
+function playclipD04() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundD04.src = "audio/D04.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("D04")[0];
+audio.play();
+}
+}
+}
+
+
+/* D05 ===============================================================*/
+
+function playclipD05() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.soundD04.src = "audio/D05.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByClassName("D05")[0];
+audio.play();
+}
+}
+}
+
+/*===============================================================*/
+
+
+/*===============================================================*/
+
+
+
